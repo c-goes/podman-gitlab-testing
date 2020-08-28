ea73b2556ad9accbc33b3ec22e4fdc45583b506c
c-goes <c-goes@users.noreply.github.com>
2020-08-28 12:23:41 +0200

    prefix lines

--- scripts/gitlab-runner.sh
+++ scripts/gitlab-runner.sh
@@ -3,7 +3,7 @@
 set -ex
 
 sudo docker info
-sudo docker --log-level debug system service -t 0 tcp:127.0.0.1:12121 &
+sudo docker --log-level debug system service -t 0 tcp:127.0.0.1:12121 | sed "s/^/[PODMAN_API] /" &
 sleep 10 && curl http://127.0.0.1:12121/images/json
 cd $GOPATH/src/gitlab.com/gitlab-runner/executors/docker
-env PATH="$PATH" sudo -E env PATH="$PATH" go test -v
+env PATH="$PATH" sudo -E env PATH="$PATH" go test -v | sed "s/^/[GITLAB_TEST] /"
