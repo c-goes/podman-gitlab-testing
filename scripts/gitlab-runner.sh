41b02ee046623a392597339346db5329f1612ee6
c-goes <c-goes@users.noreply.github.com>
2020-08-28 12:50:56 +0200

    terminate s command

--- scripts/gitlab-runner.sh
+++ scripts/gitlab-runner.sh
@@ -3,7 +3,7 @@
 set -ex
 
 sudo docker info
-sudo sh -c "podman --log-level debug system service -t 0 tcp:127.0.0.1:12121 2>&1 | sed \"s/^/[PODMAN_API] \"" &
+sudo sh -c "podman --log-level debug system service -t 0 tcp:127.0.0.1:12121 2>&1 | sed \"s/^/[PODMAN_API] /\"" &
 sleep 10 && curl http://127.0.0.1:12121/images/json
 cd $GOPATH/src/gitlab.com/gitlab-runner/executors/docker
 env PATH="$PATH" sudo -E env PATH="$PATH" go test -v | sed "s/^/[GITLAB_TEST] /"
